fbeaa9c51f687aba284f5e8840897b5e9e8191e0
PiliMachado <88661591+PiliMachado@users.noreply.github.com>
2021-10-20 07:05:56 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
 
@@ -8,6 +8,18 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider p = new PictureProvider();
+            String pathL = @"C:\Users\mielp\OneDrive\Escritorio\Programación 2\Proyectos\PipesYFilters\PII_Pipes_Filters_Start\src\Program\luke.jpg";
+            String pathB = @"C:\Users\mielp\OneDrive\Escritorio\Programación 2\Proyectos\PipesYFilters\PII_Pipes_Filters_Start\src\Program\beer.jpg";
+            PipeNull pipe1 = new PipeNull();
+            FilterNegative fNegativo = new FilterNegative();
+            PipeSerial pipe2 = new PipeSerial(fNegativo, pipe1);
+            FilterGreyscale fGris = new FilterGreyscale();
+            PipeSerial pipe3 = new PipeSerial(fGris, pipe2);
+            IPicture picture = p.GetPicture(pathL);
+            IPicture pictureEnviado = pipe3.Send(picture);
+            p.SavePicture(pipe3.Send(p.GetPicture(pathL)),pathB);
+
         }
     }
 }
